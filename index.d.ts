88c85c6b3caa6d9b33ddbc4db80d1c129d958c58
ph1p <me@ph1p.dev>
2021-01-26 00:30:12 +0100

    fix(typing): add correct typing for plugin

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,5 @@
 declare module '@paintable/vue' {
+  import { App } from 'vue';
   export interface PaintableRef {
     undo(): void;
     redo(): void;
@@ -6,4 +7,15 @@ declare module '@paintable/vue' {
     save(): void;
     clear(keepHistory?: boolean): void;
   }
+  export interface PluginOptions {
+    setItem: (key: string, value: any) => void;
+    getItem: (key: string) => any;
+    removeItem: (key: string) => void;
+  }
+
+  const PaintablePlugin: {
+    install: (app: App, options: PluginOptions) => void;
+  };
+
+  export default PaintablePlugin;
 }
